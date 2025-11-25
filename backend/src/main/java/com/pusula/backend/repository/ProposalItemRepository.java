68a47a21d083bfb40a921426dc884bf6bb83a4e6
emirrkls <mail@adresin.com>
2025-11-25 19:20:09 +0300

    fix: Reset .gitignore file

--- backend/src/main/java/com/pusula/backend/repository/ProposalItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pusula.backend.repository;
-
-import com.pusula.backend.entity.ProposalItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProposalItemRepository extends JpaRepository<ProposalItem, Long> {
-}
--- frontend-desktop/src/main/java/com/pusula/desktop/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.pusula.desktop;
-
-public class Launcher {
-    public static void main(String[] args) {
-        PusulaDesktopApp.main(args);
-    }
-}
