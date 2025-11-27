5395da1adb49be3a265752bdd1f0c42cd57dab12
emirrkls <mail@adresin.com>
2025-11-27 20:22:48 +0300

    chore: Cleanup temporary script files and notes from root directory

--- check_data.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT 'Data Check' as info;
-SELECT id, name, subscription_status FROM companies;
-SELECT id, username, role, company_id FROM users WHERE username = 'admin';
-SELECT COUNT(*) as expense_count FROM expenses WHERE company_id = 1;
-SELECT COUNT(*) as ticket_count FROM service_tickets WHERE company_id = 1;
